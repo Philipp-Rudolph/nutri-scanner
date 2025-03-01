e0c18dad510efa89710f617dfd632b0c09bcd178
Philipp-Rudolph <phil.rudolph@ymail.com>
2025-03-01 22:32:24 +0100

    fix: docker issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import path from 'path'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  define: {
+    global: 'window',
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      crypto: path.resolve(__dirname, 'node_modules/crypto-browserify'),
     },
   },
 })
